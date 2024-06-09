4d2cd2882fde7792d925220c0b0904869b8eb275
JuniorM27 <josiematonte2@gmail.com>
2024-06-08 21:57:30 -0300

    Added pytube

--- api/index.py
+++ api/index.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pytube import YouTube
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/audio")
+def getYouTubeAudio(self, link):
+    youtubeObject = YouTube(link)
+    return youtubeObject.streams.get_audio_only()
+
+@app.get("/api/video")
+def getYouTubeVideo(self, link):
+    youtubeObject = YouTube(link)
+    return youtubeObject.streams.get_audio_only()
