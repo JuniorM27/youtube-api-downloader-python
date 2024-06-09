c5a8ed9213b1d4b3ea156d39fd2df6528eb26ff5
JuniorM27 <josiematonte2@gmail.com>
2024-06-09 00:37:08 -0300

    Indexado y funcionando pero sigue sin el nombre y extension correctos.

--- api/index.py
+++ api/index.py
@@ -14,6 +14,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def download(video_stream, _media_type):
+    video_buffer = io.BytesIO()
+    video_stream.stream_to_buffer(video_buffer)
+    video_buffer.seek(0)
+    video_data = video_buffer.getvalue()
+
+    response = Response(content=video_data, media_type=_media_type)
+    response.headers["Content-Disposition"] = f"attachment; filename=video.mp4"
+    return response
+
+def stream(video_stream):
+        with io.BytesIO() as video_buffer:
+            video_stream.stream_to_buffer(video_buffer)
+            video_buffer.seek(0)
+            while True:
+                chunk = video_buffer.read(1024)
+                if not chunk:
+                    break
+                yield chunk
+
 @app.get("/api/info")
 def getYouTube(link: str):
     youtubeObject = YouTube(link)
@@ -25,47 +45,28 @@ def getYouTube(link: str):
         "video_high": FileResponse(youtubeObject.streams.get_highest_resolution(), filename=youtubeObject.title, media_type="application/octet-stream")
     }
 
+
 @app.get("/api/audio")
 def getYouTubeAudio(link: str):
     youtubeObject = YouTube(link)
     audio_stream = youtubeObject.streams.get_audio_only()
-    return generate2(audio_stream, "audio/mp3", audio_stream.title, audio_stream.subtype)
+    return download(audio_stream, "audio/mp3")
 
-def generate2(video_stream, _media_type, titulo="video", ext="mp4"):
-    video_buffer = io.BytesIO()
-    video_stream.stream_to_buffer(video_buffer)
-    video_buffer.seek(0)
-    video_data = video_buffer.getvalue()
-
-    response = Response(content=video_data, media_type=_media_type)
-    response.headers["Content-Disposition"] = f"attachment; filename={titulo}.{ext}"
-    
-    return response
-
-def generate(video_stream):
-        with io.BytesIO() as video_buffer:
-            video_stream.stream_to_buffer(video_buffer)
-            video_buffer.seek(0)
-            while True:
-                chunk = video_buffer.read(1024)
-                if not chunk:
-                    break
-                yield chunk
 
 @app.get("/api/video")
 def getYouTubeVideo(link: str):
     youtubeObject = YouTube(link)
     video_stream = youtubeObject.streams.get_highest_resolution()
-    return generate2(video_stream, "video/mp4", video_stream.title, video_stream.subtype)
+    return download(video_stream, "video/mp4")
 
 @app.get("/api/stream/video")
 def getYouTubeStreamVideo(link: str):
     youtubeObject = YouTube(link)
     video_stream = youtubeObject.streams.get_highest_resolution()
-    return StreamingResponse(generate(video_stream), media_type="video/mp4")
+    return StreamingResponse(stream(video_stream), media_type="video/mp4")
 
 @app.get("/api/stream/audio")
 def getYouTubeStreamAudio(link: str):
     youtubeObject = YouTube(link)
     video_stream = youtubeObject.streams.get_audio_only()
-    return StreamingResponse(generate(video_stream), media_type="video/mp4")
+    return StreamingResponse(stream(video_stream), media_type="video/mp4")
